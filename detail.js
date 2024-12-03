c9c3ba2af94dca08ba3699936b2a3eaf403b853e
balqisrosa <balqisrosasekamayang@gmail.com>
2024-12-03 13:54:34 +0700

    up

--- detail.js
+++ detail.js
@@ -1,7 +1,6 @@
 const products = {
     "kopisusu": {
         name: "Kopi Susu",
-        description: "A delightful mix of rich coffee and creamy milk, perfect for any time of the day.",
         about: "Kopi susu adalah campuran kopi espresso dan susu segar, sering diberi tambahan gula atau sirup untuk rasa manis.",
         price: "Rp. 16.000",
         image: "bg/kopsu.png",
@@ -9,7 +8,6 @@ const products = {
     },
     "vanillabliss": {
         name: "Vanilla Bliss",
-        description: "A smooth blend of coffee with a fragrant hint of vanilla.",
         about: "Minuman ini dibuat dari kopi, susu, dan sirup vanila yang memberikan rasa manis harum.",
         price: "Rp. 15.000",
         image: "bg/vanilla_bliss.png",
@@ -17,7 +15,6 @@ const products = {
     },
     "machastrawberry": {
         name: "Macha Strawberry",
-        description: "A refreshing combination of matcha and strawberry.",
         about: "Minuman ini memadukan matcha premium dengan stroberi segar, menciptakan rasa segar yang unik.",
         price: "Rp. 18.000",
         image: "bg/sm.png",
@@ -25,18 +22,16 @@ const products = {
     },
     "camarillo": {
         name: "Camarillo",
-        description: "A rich and smooth coffee blended with caramel and milk, a perfect treat for caramel lovers.",
         about: "Camarillo adalah perpaduan kopi yang kaya dengan karamel manis dan susu, menciptakan rasa lembut dan gurih.",
         price: "Rp. 17.000",
-        image: "bg/camarillo.png",
+        image: "bg/Camarillo.png",
         orderLink: "https://logiccoffee.github.io/menu/"
     },
     "berrydream": {
         name: "Berry Dream",
-        description: "A fruity fusion of berries and coffee, perfect for a refreshing treat.",
         about: "Berry Dream adalah kombinasi kopi dengan campuran buah beri yang segar, menawarkan rasa manis dan asam yang seimbang.",
         price: "Rp. 19.000",
-        image: "bg/berrydream.png",
+        image: "bg/berry_dream.png",
         orderLink: "https://logiccoffee.github.io/menu/"
     }
 };
